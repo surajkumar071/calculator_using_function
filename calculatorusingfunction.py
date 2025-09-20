9bce8731b06feb77ed47b40262dcefc179f9d9e2
Suraj Kumar <surajk2072003@gmail.com>
2025-09-20 13:47:42 +0530

    Create calculatorusingfunction.py

---
+++ calculatorusingfunction.py
@@ -0,0 +1,33 @@
+num1 = int(input("Enter a First Number: "))
+num2 = int(input("Enter a Second Number: "))
+operations = ['add', 'sub', 'multiply', 'divide']
+c = input("Choose Your Operation (add, sub, multiply, divide) : ")
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+result = None
+if c in operations:
+    if c == 'add':
+        result = add(num1, num2)
+    elif c == 'sub':
+        result = sub(num1, num2)
+    elif c == 'multiply':
+        result = multiply(num1, num2)
+    elif c == 'divide':
+        result = divide(num1, num2)
+    print(f"The result of {c} is: {result}")
+else:
+    print("Invalid operation chosen.")
